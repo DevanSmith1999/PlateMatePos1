9b5fd8ee5dd40fb6e3eb53171498c3d8e35fc207
Devansmith1999 <smithdevan1999@gmail.com>
2024-03-04 13:30:08 -0600

    Added MenuItems folder with models

---
+++ MenuItems/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+class CustomCategory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Ingredient(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class FoodItem(models.Model):
+    category = models.ForeignKey(CustomCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    ingredients = models.ManyToManyField(Ingredient, related_name="foods", blank=True)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class DrinkItem(models.Model):
+    category = models.ForeignKey(CustomCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class LBWItem(models.Model):
+    category = models.ForeignKey(CustomCategory, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name}"
