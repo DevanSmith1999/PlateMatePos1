829443d19e166c708a05a53718312b86ca567422
jaylon <jonesjaylon725@gmail.com>
2024-03-26 14:26:32 -0500

    Changed migrate command to automatically import Menu item fixture after completing migration

--- manage.py
+++ manage.py
@@ -9,6 +9,9 @@ def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_project.settings")
     try:
         from django.core.management import execute_from_command_line
+
+        
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
@@ -16,6 +19,9 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+    if len(sys.argv) == 2 and sys.argv[1] == 'migrate': #Auto Imports Fixture to populate Menu Items Model
+            execute_from_command_line(['manage.py', 'loaddata', 'MenuItems'])
 
 
 if __name__ == "__main__":
