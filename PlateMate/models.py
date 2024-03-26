f1aefb03c6c735d599f2c5dcb9aefd4483f17825
jaylon <jonesjaylon725@gmail.com>
2024-03-26 14:03:00 -0500

    Added import in veiws for accessing menuitem data

--- PlateMate/models.py
+++ PlateMate/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+#To Populate model with data from PlateMate/fixtures/MenuItems.json
+# Enter the command: "python manage.py loaddata Menuitems"
 class MenuItem(models.Model):
   Name = models.CharField( ("Name"), max_length=100)
   Type = models.CharField( ("type"), max_length=50)
