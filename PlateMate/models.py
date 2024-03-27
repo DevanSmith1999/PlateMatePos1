e9c5a295d9f81dc7a57cbbf826a4e218b51897f4
jaylon <jonesjaylon725@gmail.com>
2024-03-27 12:36:15 -0500

    Remove Useless comment in models.py

--- PlateMate/models.py
+++ PlateMate/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-#To Populate model with data from PlateMate/fixtures/MenuItems.json
-# Enter the command: "python manage.py loaddata Menuitems"
 class MenuItem(models.Model):
   Name = models.CharField( ("Name"), max_length=100)
   Type = models.CharField( ("type"), max_length=50)
