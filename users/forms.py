085823e0ffa45ce9b1ae81f0f8cd6b4aa18a1821
Devansmith1999 <smithdevan1999@gmail.com>
2024-04-02 14:51:56 -0500

    Completely done with signup and login in functionality. Users can't create multiple accounts with same username or password. Users must be an active user to login. A few things to be worked on are adding the logo and a navbar potentially.

--- users/forms.py
+++ users/forms.py
@@ -5,15 +5,28 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
-
     class Meta:
+
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        help_texts = {
+            'username': None,
+            'email':None,
+        }
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Set help_texts for password fields to empty dictionary
+        self.fields['password2'].help_text=None
+       
 
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.email = self.cleaned_data['email']
-        if commit:
-            user.save()
-        return user
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("Username is already taken.")
+        return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Email address is already registered.")
+        return email
--- users/views.py
+++ users/views.py
@@ -13,8 +13,17 @@ def signup(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('login')
+            username = form.cleaned_data.get('username')
+            email = form.cleaned_data.get('email')
+            if User.objects.filter(username=username).exists():
+                error_message = "Username is already taken."
+                return render(request, 'signup.html', {'form': form, 'error_message': error_message})
+            elif User.objects.filter(email=email).exists():
+                error_message = "Email address is already registered."
+                return render(request, 'signup.html', {'form': form, 'error_message': error_message})
+            else:
+                form.save()
+                return redirect('login')
     else:
         form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
