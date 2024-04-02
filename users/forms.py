62e2385c2273c4a364f07ddc657a0cc1d85702dc
Devansmith1999 <smithdevan1999@gmail.com>
2024-04-02 14:09:14 -0500

    Changed the UI of the login and signup page

--- users/forms.py
+++ users/forms.py
@@ -1,30 +1,19 @@
+# forms.py
+
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-class SignUpForm(forms.Form):
-    username = forms.CharField(max_length=150)
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
 
-    def clean_username(self):
-        username = self.cleaned_data['username']
-        if User.objects.filter(username=username).exists():
-            raise forms.ValidationError("Username is already taken.")
-        return username
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
-    def clean_confirm_password(self):
-        password = self.cleaned_data.get('password')
-        confirm_password = self.cleaned_data.get('confirm_password')
-
-        if password and confirm_password and password != confirm_password:
-            raise forms.ValidationError("Passwords do not match.")
-        
-        return confirm_password
-
-    def save(self):
-        username = self.cleaned_data['username']
-        email = self.cleaned_data['email']
-        password = self.cleaned_data['password']
-        user = User.objects.create_user(username=username, email=email, password=password)
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
         return user
--- users/views.py
+++ users/views.py
@@ -10,20 +10,11 @@ def home(request):
  return render(request, "PlateMate/home.html", {})
 
 def signup(request):
-    error_message = None
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            if User.objects.filter(username=username).exists():
-                error_message = "Username is already taken."
-            elif User.objects.filter(email=email).exists():
-                error_message = "Email address is already registered."
-            else:
-                user = User.objects.create_user(username=username, email=email, password=password)
-                return redirect('login')
+            form.save()
+            return redirect('login')
     else:
         form = SignUpForm()
-    return render(request, 'registration/signup.html', {'form': form, 'error_message': error_message})
+    return render(request, 'registration/signup.html', {'form': form})
